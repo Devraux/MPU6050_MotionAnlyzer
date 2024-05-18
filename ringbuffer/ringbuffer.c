d69d6fcfe88f3f54b7cbb2e8c855aae0edcef649
Devraux <krzysztof.plonka2@onet.pl>
2024-05-18 11:10:33 +0200

    ring buffer done

--- ringbuffer/ringbuffer.c
+++ ringbuffer/ringbuffer.c
@@ -1,6 +1,35 @@
 #include "ringbuffer.h"
 
-void Ring_buffer_init(RINGBUFFER* ringbuffer);
-void Ring_buffer_add(RINGBUFFER* ringbuffer);
-void Ring_buffer_get_capacity(RINGBUFFER* ringbuffer);
-void Ring_buffer_clear(RINGBUFFER* ringbuffer);
+void Ring_buffer_init(RINGBUFFER* ringbuffer, float* data, uint16_t buffer_size)
+{
+    ringbuffer->Buffer_Size = buffer_size;
+    ringbuffer->Data = data;
+    ringbuffer->Head = 0;
+    ringbuffer->Tail = 0;
+}
+
+void Ring_buffer_push(RINGBUFFER* ringbuffer, float data)
+{
+    ringbuffer->Data[ringbuffer->Head] = data;
+    ringbuffer->Head = (ringbuffer->Head + 1) % ringbuffer->Buffer_Size;
+    ringbuffer->Counter++;
+}
+
+void Ring_buffer_pop(RINGBUFFER* ringbuffer, float* data)
+{
+    *data = ringbuffer->Data[ringbuffer->Tail];
+    ringbuffer->Tail = (ringbuffer->Tail + 1) % ringbuffer->Buffer_Size;
+    ringbuffer->Tail--;
+}
+
+uint16_t Ring_buffer_get_capacity(RINGBUFFER* ringbuffer)
+{
+    return ringbuffer->Counter;
+}
+
+void Ring_buffer_clear(RINGBUFFER* ringbuffer)
+{
+    ringbuffer->Counter = 0;
+    ringbuffer->Head = 0;
+    ringbuffer->Tail = 0;
+}
--- ringbuffer/ringbuffer.h
+++ ringbuffer/ringbuffer.h
@@ -8,16 +8,17 @@
 
 typedef struct RINGBUFFER
 {
-    float* Ringbuffer;  //ring buffer data
-    uint16_t Counter;     //counts data in buffer
+    float* Data;  //ring buffer data
+    uint16_t Counter;   //counts data in buffer
     uint16_t Head;    
     uint16_t Tail;
     uint16_t Buffer_Size;
 }RINGBUFFER;
 
-void Ring_buffer_init(RINGBUFFER* ringbuffer);
-void Ring_buffer_add(RINGBUFFER* ringbuffer);
-void Ring_buffer_get_capacity(RINGBUFFER* ringbuffer);
+void Ring_buffer_init(RINGBUFFER* ringbuffer, float* data, uint16_t buffer_size);
+void Ring_buffer_push(RINGBUFFER* ringbuffer, float data);
+void Ring_buffer_pop(RINGBUFFER* ringbuffer, float* data);
+uint16_t Ring_buffer_get_capacity(RINGBUFFER* ringbuffer);
 void Ring_buffer_clear(RINGBUFFER* ringbuffer);
 
 
